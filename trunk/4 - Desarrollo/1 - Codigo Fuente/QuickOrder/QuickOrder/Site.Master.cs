01e18ee500a5b4906cbbb887b0cfed81e21f8d70
ivdavaro <ivdavaro@009374dc-1a64-3be8-7608-6c4100ca8900>
2013-04-21 20:33:08 +0000

    LAOZ - cambio sefectuados en el proyecto y traduccion a español

--- trunk/4 - Desarrollo/1 - Codigo Fuente/QuickOrder/QuickOrder/Site.Master.cs
+++ trunk/4 - Desarrollo/1 - Codigo Fuente/QuickOrder/QuickOrder/Site.Master.cs
@@ -17,14 +17,22 @@ namespace QuickOrder
         /// <param name="e">Evento</param>
         protected void Page_Init(object sender, EventArgs e)
         {
+            Validarusuario();
+            Page.PreLoad += master_Page_PreLoad;
+        }
+
+        /// <summary>
+        /// Valida el nombre de usuario
+        /// </summary>
+        private void Validarusuario() {
             var usuario = (VoQuickOrder.Usuario)Session["usuario"];
 
-            if (usuario != null)
-            {
-                lbNombreUsuario.Text = usuario.NombreUsuario;
-            }          
+            var validacion = usuario != null;
 
-            Page.PreLoad += master_Page_PreLoad;
+            lbNombreUsuario.Text = validacion ? usuario.NombreUsuario : string.Empty;
+            lnkblogin.Visible = !validacion;
+            lnkbregister.Visible = !validacion;
+            btnSalir.Visible = validacion;
         }
 
         /// <summary>
@@ -45,6 +53,27 @@ namespace QuickOrder
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        /// <summary>
+        /// Evento del boton salir
+        /// </summary>
+        /// <param name="sender">Objeto que genera el evento</param>
+        /// <param name="e">Evento</param>
+        protected void btnSalir_Click(object sender, EventArgs e)
+        {
+            Session["usuario"] = null;
+            Validarusuario();
+        }
+
+        protected void lnkblogin_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Login.aspx");
+        }
+
+        protected void lnkbregister_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Register.aspx");
         }
     }
 }
